0466faa770476250e375e66a1cfd95ce98cb1cf5
Davi <144630222+Davi-Cyber-Security@users.noreply.github.com>
2025-06-09 23:39:18 +0000

    Atualiza comentários e estrutura do código para melhor legibilidade e entendimento das funções

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+// Importando pacotes necessários para o funcionamento do sistema de busca
 import express from 'express';
 import cors from 'cors';
 import { buscaTotal, buscaID, buscarCausa, buscarDiagnostico, buscarMedicamento, buscarRelatoPaciente, buscarSintomas, buscarTratamento, buscaCompleta } from './servico/servico.js';
 
+
+// Declarando as funções de configuraçõs do sistema
 const app = express();
 app.use(cors()); // Adicionando a função para aceitar novas requisições de outros domínios
 const PORT = process.env.PORT || 8080;
 
+
+// Declarando que o servidor vai aceitar requisições do tipo json e urlencoded
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public')); // Aceitando modelos html e css
 app.use(express.json()); // Aceitando json
 
+
+// Abrindo uma endpoint para receber a requisição post do fuse.js
 app.post('/busca', (req, res) => {
     const termo = req.body.pesquisa;
     
@@ -25,10 +32,11 @@ app.post('/busca', (req, res) => {
     }
 })
 
+// Outra endpoint para receber requisições normais de get. Ou seja, ele vai receber os parâmetros diretamente na URL
 app.get('/buscadiagnostico', (req, res) => {
 
     const{pesquisa, relatoPaciente, sintomas, diagnostico, causas, tratamento, medicamento, busca} = req.query;
-
+    // Adicionando tratamento de exceções
     try{
     const buscas =[
         ...(relatoPaciente ? buscarRelatoPaciente(relatoPaciente) : []),
@@ -56,6 +64,7 @@ app.get('/buscadiagnostico', (req, res) => {
 });
 
 
+// Escutando o servidor na porta definida!
 app.listen(PORT, () =>{
     const data = new Date();
     console.log(`🟢 Servidor em node.js iniciado em: ${data.toLocaleDateString()} às ${data.toLocaleTimeString()}`);
--- servico/servico.js
+++ servico/servico.js
@@ -1,21 +1,27 @@
+// Importando as bibliotecas necessárias para o funcionamento do sistema de busca com fuse.js
 import Fuse from 'fuse.js';
 import {dados as dados} from '../dados/dados.js';
 
+// Declarando as opções de configuração do fuse.js. Ou seja, estamos utilizando 0.4 como threshold, o que significa que a busca será mais flexível, permitindo resultados com uma certa margem de erro. Facilitando a busca por palavras-chave em várias chaves do objeto de dados, como relatoPaciente, diagnostico, sintomas, causas, tratamento e medicamento.
 const fuse = new Fuse (dados, {
     includeScore: true,
     keys: ['relatoPaciente', 'diagnostico', 'sintomas', 'causas', 'tratamento', 'medicamento'],
     threshold: 0.4,
 });
 
+
+// Função de buscar e trazer todos os dados (informações) do arquivo dados.js
 export function buscaTotal() {
     return dados;
 };
 
+// Função de pesquisa completa que utiliza o fuse.js para buscar informações com base no termo de pesquisa fornecido. Ela retorna um array de objetos que correspondem ao termo pesquisado.
 export function buscaCompleta(pesquisa){
     const resultado = fuse.search(pesquisa);
     return resultado.map(res => res.item);
 }
 
+// O restante abaixo são funções específicas de busca. Utilizado para a opção de selecionar.
 export function buscarRelatoPaciente (B){
     return dados.filter(busca => busca.relatoPaciente.toLowerCase().includes(B.toLowerCase()));
 }
@@ -40,6 +46,7 @@ export function buscarMedicamento (M){
     return dados.filter(busca => busca.medicamento.toLowerCase().includes(M.toLowerCase()));
 }
 
+// Função de busca por ID
 export function buscaID(id){
     const verificador = parseInt(id);
     return dados.find(dados => dados.id === verificador);
